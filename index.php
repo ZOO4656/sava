8f5e6251eeba6817ef0a003bbf2f999486f1fa63
uenoryo <r.r.ag0611@outlook.jp>
2017-03-12 15:31:01 +0900

    first commit.

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+    $sava_list = [
+        '塩サバ',
+        'サバの味噌煮',
+        'サバ寿司',
+        'サバカレー',
+        'サババーガー',
+        'サバうどん',
+        'サバ焼きそば'
+    ];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <section>
+        <h1>今週のこんだて</h1>
+            <ul>
+                <?php foreach ($sava_list as $sava) :?>
+                    <li>
+                        <?= $sava ?>
+                    </li>
+                <?php endforeach ?>
+            </ul>
+    </section>
+</body>
+</html>
